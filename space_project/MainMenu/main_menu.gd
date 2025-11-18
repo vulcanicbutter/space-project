3e0380cf3d087f768fc7ae712409a47630de08ba
vulcanicbutter <aron.mihalyi2008@gmail.com>
2025-11-18 15:32:54 +0100

    Add files via upload

---
+++ space_project/MainMenu/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var play_button = $Play
+@onready var options_button = $Options
+@onready var exit_button = $Quit
+
+# Játék indítása
+func _on_play_pressed():
+	print("Játék elindítva...")
+	var scene = load("res://Lobby/Lobby.tscn")
+	get_tree().change_scene_to_packed(scene)  # A játék fő scene-je
+
+# Beállítások menü
+func _on_options_pressed():
+	print("Beállítások menü...")
+	var scene = load("res://MainMenu/Main Menu.tscn")
+	# Ha van beállítások scene
+	get_tree().change_scene_to_packed(scene)
+
+# Kilépés
+func _on_quit_pressed():
+	print("Kilépés...")
+	get_tree().quit()  # Kilép a játékból
---
+++ space_project/items/gun/Bullet/bullet.gd
@@ -0,0 +1,9 @@
+extends Node2D
+@export var Bulletspeed = 300
+
+func _process(delta: float) -> void:
+	position += transform.x * Bulletspeed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
